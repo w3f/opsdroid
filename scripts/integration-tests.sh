d1159bc1b18ed061bccd2c2eb59b26e4ca581ef9
pantelis <pampatzoglou@gmail.com>
2020-12-08 11:48:57 +0200

    initial push

---
+++ scripts/integration-tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+source /scripts/common.sh
+source /scripts/bootstrap-helm.sh
+
+run_tests() {
+    echo Running tests...
+
+    POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=opsdroid,app.kubernetes.io/instance=opsdroid" -o jsonpath="{.items[0].metadata.name}")
+    wait_pod_ready $POD_NAME
+
+}
+
+main(){
+    /scripts/build-helmfile.sh
+    helm install --generate-name ./chart/opsdroid
+    run_tests
+}
+
+main
